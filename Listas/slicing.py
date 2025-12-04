e763f415250dbd97f48559156e222a254521c164
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-04 08:12:28 -0300

    Create slicing.py

---
+++ Listas/slicing.py
@@ -0,0 +1,16 @@
+cores = ["vermelho", "verde", "azul", "amarelo", "laranja", "roxo", "marrom", "cinza"]
+
+cor = cores[1: 3]
+print(cor)
+
+duas_primeiras = cores[: 2]
+print(duas_primeiras)
+
+apartir_amarelo = cores[4: ]
+print(apartir_amarelo)
+
+posicao_impar = cores[1 : :2]
+print(posicao_impar)
+
+invertido = cores[ : :-1]
+print(invertido)
