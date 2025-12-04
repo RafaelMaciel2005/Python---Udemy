e4f718994b5f31819f8a5db3fc50c22c7e4cef01
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-04 10:54:15 -0300

    Create ex05.py

---
+++ Listas/ex05.py
@@ -0,0 +1,18 @@
+valores = [23, 45, 67, 89, 12, 56, 78, 90, 34, 56]
+
+quantidade = len(valores)
+print("A quantidade de valores na lista é {}".format(quantidade))
+
+valor_maximo = max(valores)
+print("O valor maximo da lista é {}".format(valor_maximo))
+
+valor_minimo = min(valores)
+print("O valor minimo da lista é {}".format(valor_minimo))
+
+soma_valores = sum(valores)
+print("A soma dos valores é {}".format(soma_valores))
+
+lista_nova = [58.5, 63.2, 71.3, 69.4, 68.2]
+
+media = sum(lista_nova) / len(lista_nova)
+print("A media dessa lista é {}.".format(media))
