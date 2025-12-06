ee1c3c19fad9983d357384ce881b735c05faf62a
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-06 04:06:32 -0300

    Create ex06.py

---
+++ Listas/ex06.py
@@ -0,0 +1,11 @@
+palavra = "Python"
+lista_palavra = list(palavra)
+print(lista_palavra)
+
+frase = "Aprender Python é divertido!"
+lista_frase = ''.join(list(frase))
+print(lista_frase)
+
+itens = ["Maçã", "Banana", "Cereja"]
+itens_lista = ', '.join(itens)
+print(itens_lista)
