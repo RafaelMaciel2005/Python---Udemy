ad9ff2ca5cd4887552129a874e465eeeffaae49b
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-08 02:19:30 -0300

    Update ex01.py

---
+++ Dicionários/ex01.py
@@ -0,0 +1,23 @@
+animal = {
+    "tipo": "gato",
+    "cor": "branco",
+    "idade": 3
+}
+print(animal["cor"])
+
+estudante = {
+    "Nome": "Carlos",
+    "Idade": 22,
+    "Curso": "Matemática",
+    "Semestre": "2"
+}
+
+Universidade = {
+    "Nome": "Universidade Federal",
+    "Localidade": {
+        "Cidade": "Rio de Janeiro",
+        "Bairro": "Centro"
+    }
+}
+print(Universidade["Nome"])
+print(Universidade["Localidade"]["Cidade"])
