d45333cc20dac12a9a16d8c8ba670de7771436bd
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-15 19:20:20 -0300

    Update robo1_ex02.py

--- Robôs/Robô1/robo1_ex02.py
+++ Robôs/Robô1/robo1_ex02.py
@@ -1,32 +1,32 @@
 import pyautogui as posicaoAbrirGoogle
 
 #Tempo de espera para o computador pensar
-posicaoAbrirGoogle.sleep(4)
+posicaoAbrirGoogle.sleep(2)
 
 
 #Clicamos no botão Start ou seja no botão do Windows
 posicaoAbrirGoogle.doubleClick(x=111, y=39)
 
 #Tempo de espera para o computador pensar
-posicaoAbrirGoogle.sleep(6)
+posicaoAbrirGoogle.sleep(3)
 
 #Digitamos o site do google
 posicaoAbrirGoogle.typewrite('https://www.google.com/')
 
 #Tempo de espera para o computador pensar
-posicaoAbrirGoogle.sleep(3)
+posicaoAbrirGoogle.sleep(2)
 
 #Apertamos a tecla enter
 posicaoAbrirGoogle.press('enter')
 
 #Tempo de espera para o computador pensar
-posicaoAbrirGoogle.sleep(7)
+posicaoAbrirGoogle.sleep(3)
 
 #Digitamos o site do google
 posicaoAbrirGoogle.typewrite('Dolar hoje')
 
 #Tempo de espera para o computador pensar
-posicaoAbrirGoogle.sleep(3)
+posicaoAbrirGoogle.sleep(2)
 
 #Apertamos a tecla enter
 posicaoAbrirGoogle.press('enter')
