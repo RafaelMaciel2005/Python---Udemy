d7a0375ca8ece71b0a33fe04745a30b417b56172
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-15 11:33:28 -0300

    Create conta_bancaria.py

---
+++ Programação Orientada a Obejtos(POO)/conta_bancaria.py
@@ -0,0 +1,29 @@
+class conta_bancaria:
+    def __init__(self, saldo):
+        self._saldo = 0.0
+        self.saldo = saldo
+
+    @property
+    def saldo(self):
+
+        return self._saldo    
+
+    @saldo.setter 
+    def saldo(self, valor):
+
+        if valor >= 0.0:           
+            self._saldo = valor
+        else:
+            print("Valor Inválido")       
+
+    def mostrar_saldo(self):
+        print(self.saldo)    
+
+conta = conta_bancaria(100)         
+conta.mostrar_saldo()        
+
+conta.saldo = 250
+conta.mostrar_saldo()
+
+conta.saldo = -50   # inválido
+conta.mostrar_saldo()
