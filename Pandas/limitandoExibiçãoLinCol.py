549673abf165dab64fc88762e0eb1375646b4320
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-16 14:59:40 -0300

    Update limitandoExibiçãoLinCol.py

--- Pandas/limitandoExibiçãoLinCol.py
+++ Pandas/limitandoExibiçãoLinCol.py
@@ -3,4 +3,12 @@ from IPython.display import display
 
 vendas_DataFrame = pd.read_excel(r"C:\Users\Rafael\Downloads\Vendas_Jan.xlsx")
 
-display(vendas_DataFrame)
+#display(vendas_DataFrame)
+
+#display(vendas_DataFrame.index)
+
+#display(vendas_DataFrame.columns)
+
+#display(vendas_DataFrame.head)
+
+display(vendas_DataFrame[["Vendedor", "Total Vendas"]])
