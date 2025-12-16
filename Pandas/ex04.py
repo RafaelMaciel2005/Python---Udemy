8cc7cc3d4524bb05cabfb6ceb968e2132d663864
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-15 23:25:26 -0300

    Create ex04.py

---
+++ Pandas/ex04.py
@@ -0,0 +1,10 @@
+import pandas as opcoesPandas
+import numpy as opcoesNumpy
+
+notasAlunos_DF = opcoesPandas.DataFrame({
+
+    "Nome": ["Ana", "Pedro", "João"],
+    "Média": [9, 7, 10]
+
+})
+print(notasAlunos_DF)
