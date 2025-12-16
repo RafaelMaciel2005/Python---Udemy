e62b8534ed908b72a5cade0d57a5c1e56ee1395d
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-16 15:42:28 -0300

    Update deletandoLinCol.py

--- Pandas/deletandoLinCol.py
+++ Pandas/deletandoLinCol.py
@@ -3,6 +3,13 @@ from IPython.display import display
 
 dataFrameDados = opcoesPandas.read_excel(r"C:\\Users\\Rafael\Documents\\Estudos\\Python-Curso-Udemy\\Arquivos fonte de estudos\\Deletar_Linhas_Colunas.xlsx")
 
-deletandoLinhasEmBranco = dataFrameDados.dropna()
+#deletandoLinhasEmBranco = dataFrameDados.dropna()
 
-display(deletandoLinhasEmBranco) 
+#del deletandoLinhasEmBranco["Produto"]
+#deletarDuasColunas = dataFrameDados.drop(columns=["Produto", "Data Venda"])
+
+#display(deletandoLinhasEmBranco) 
+#display(deletarDuasColunas)
+
+excluirLinha3 = dataFrameDados.drop(2, axis=0)
+display(excluirLinha3)
