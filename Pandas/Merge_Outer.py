b6019c246d884c7b56360b670fd61dc81c268f21
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-16 22:45:35 -0300

    Update Merge_Outer.py

--- Pandas/Merge_Outer.py
+++ Pandas/Merge_Outer.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from IPython.display import display
 
-vendas_DF = pd.read_excel(r"C:\Users\Rafael\Documents\Estudos\Python-Curso-Udemy\Arquivos fonte de estudos\\Vendas_LEFT_JOIN.xlsx")
+Loja1_DF = pd.read_excel(r"C:\Users\Rafael\Documents\Estudos\Python-Curso-Udemy\Arquivos fonte de estudos\\Outer_Vendas_Loja1.xlsx")
 
-vendedores_DF = pd.read_excel(r"C:\Users\Rafael\Documents\Estudos\Python-Curso-Udemy\Arquivos fonte de estudos\\Vendedores_LEFT_JOIN.xlsx")
+Loja2_DF = pd.read_excel(r"C:\Users\Rafael\Documents\Estudos\Python-Curso-Udemy\Arquivos fonte de estudos\\Outer_Vendas_Loja2.xlsx")
+
+verificandoVendas_DF = pd.merge(Loja1_DF, Loja2_DF, on=["Id Vendedor"], how="outer", suffixes=("Loja1", "Loja2"))
+
+display(verificandoVendas_DF)
