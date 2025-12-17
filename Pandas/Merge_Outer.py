b1644eeb5bb37803ed97c0f470b2fdf6ae9a3efe
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-16 22:40:42 -0300

    Create Merge_Outer.py

---
+++ Pandas/Merge_Outer.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from IPython.display import display
+
+vendas_DF = pd.read_excel(r"C:\Users\Rafael\Documents\Estudos\Python-Curso-Udemy\Arquivos fonte de estudos\\Vendas_LEFT_JOIN.xlsx")
+
+vendedores_DF = pd.read_excel(r"C:\Users\Rafael\Documents\Estudos\Python-Curso-Udemy\Arquivos fonte de estudos\\Vendedores_LEFT_JOIN.xlsx")
