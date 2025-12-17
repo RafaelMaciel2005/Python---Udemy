13f29115c1c9763d0e7683ba5e8a90307d68a709
Rafael Maciel <maciel.rafael2005@gmail.com>
2025-12-17 12:36:51 -0300

    Update pivot_Table.py

--- Pandas/pivot_Table.py
+++ Pandas/pivot_Table.py
@@ -3,6 +3,6 @@ from IPython.display import display
 
 baseLanchonete_DF = pd.read_excel(r"C:\Users\Rafael\Documents\Estudos\Python-Curso-Udemy\Arquivos fonte de estudos\Vendas_Lanchonete_Pivot_Table.xlsx")
 
-pivotExemplo1 = baseLanchonete_DF.pivot(index="Data Venda", columns="Cliente", values="Preço")
+pivotExemplo1 = baseLanchonete_DF.pivot_table(index="Data Venda", columns="Cliente", values="Preço com Desconto", aggfunc="sum")
 
 display(pivotExemplo1)
